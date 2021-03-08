26e7a26f61dbd2d10324553852614eada8c2751f
AP7557 <ap.akash.patel1999+github@gmail.com>
2021-03-07 20:41:11 -0500

    part2

---
+++ app/src/main/java/com/example/parstgram/fragments/PostsFragment.java
@@ -0,0 +1,97 @@
+package com.example.parstgram.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.parstgram.Post;
+import com.example.parstgram.PostsAdapter;
+import com.example.parstgram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PostsFragment extends Fragment {
+
+    public static final String TAG = "PostsFragment";
+
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+    SwipeRefreshLayout swipeContainer;
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i(TAG, "fetching new data");
+                queryPosts();
+            }
+        });
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(),allPosts);
+
+        //Steps to use recycler view:
+        // 0. create layout for 1 row in the list
+        // 1. create adapter
+        // 2. create data source
+        // 3. set adapter on recycler view
+        rvPosts.setAdapter(adapter);
+        // 4. set layout manager on recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+
+    }
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for(Post post: posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username " + post.getUser().getUsername());
+                }
+                adapter.clear();
+                adapter.addAll(posts);
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+}
